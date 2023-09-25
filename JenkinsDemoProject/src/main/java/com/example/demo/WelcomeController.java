49a2a36d48c4cb01e27a544ac1976c19132561ec
ketand <ketand@cdac.in>
2023-09-25 11:47:36 +0530

    First Commit-jenkins project added

---
+++ JenkinsDemoProject/src/main/java/com/example/demo/WelcomeController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import ch.qos.logback.core.model.Model;
+
+@Controller
+public class WelcomeController {
+	
+	@GetMapping("/welcome")
+	@ResponseBody
+	public String Welcome(Model model) {
+		return "Welcome TO My World!!!";
+	}
+
+}
