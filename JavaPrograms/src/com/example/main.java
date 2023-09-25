9dd29b93ee039dd333fd476a5a7f6ec49d8106ef
ketand <ketand@cdac.in>
2023-09-25 12:46:07 +0530

    new class added

---
+++ JavaPrograms/src/com/example/main.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.print("Hey...Hello from SpringBoot");
+	}
+
+}
